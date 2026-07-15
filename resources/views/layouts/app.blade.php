1462704c468fb744e0caeba8d8f7474deec617d4
sandeep8558 <sandeep198558@gmail.com>
2026-07-15 13:10:04 +0530

    Remove max-width limits to make Dashboard header and content full-width

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
                 <!-- Page Heading -->
                 @if (isset($header))
                     <header class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-                        <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+                        <div class="py-4 px-4 sm:px-6 lg:px-8">
                             {{ $header }}
                         </div>
                     </header>
