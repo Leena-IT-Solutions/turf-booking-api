e4a1701914ad6d6ccda570e4fcb8fe73700a6f2b
sandeep8558 <sandeep198558@gmail.com>
2026-07-15 18:09:25 +0530

    Wipe old logos folder and seed brand logo image at start of database seeding

--- database/seeders/SaasSettingSeeder.php
+++ database/seeders/SaasSettingSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\SaasSetting;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class SaasSettingSeeder extends Seeder
 {
@@ -12,13 +13,43 @@ class SaasSettingSeeder extends Seeder
      */
     public function run(): void
     {
-        SaasSetting::firstOrCreate([
+        $publicDisk = Storage::disk('public');
+        
+        // Clean up logos directory to prevent stale storage accumulation
+        if ($publicDisk->exists('logos')) {
+            $publicDisk->deleteDirectory('logos');
+        }
+        $publicDisk->makeDirectory('logos');
+
+        $logoPath = 'logos/brand_logo.png';
+        if (extension_loaded('gd')) {
+            $im = imagecreatetruecolor(200, 200);
+            $bg = imagecolorallocate($im, 79, 70, 229); // Indigo 600
+            imagefill($im, 0, 0, $bg);
+            
+            $white = imagecolorallocate($im, 255, 255, 255);
+            imagefilledellipse($im, 100, 100, 120, 120, $white);
+            
+            imagefilledellipse($im, 100, 100, 60, 60, $bg);
+
+            ob_start();
+            imagepng($im);
+            $imageData = ob_get_clean();
+            imagedestroy($im);
+            
+            $publicDisk->put($logoPath, $imageData);
+        } else {
+            $publicDisk->put($logoPath, 'dummy logo content');
+        }
+
+        SaasSetting::updateOrCreate([
             'id' => 1,
         ], [
             'app_name' => 'TurfBooking',
             'contact_email' => 'sandeep198558@gmail.com',
             'contact_mobile' => '9664588677',
             'address' => 'Mumbai, India',
+            'logo_path' => $logoPath,
             'is_maintenance_mode' => false,
         ]);
     }
